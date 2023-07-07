f3742abc79b2546fb37cb0572e0aa65e546e59cb
Darren Washington <dsw@meta.com>
2023-07-07 10:10:28 -0700

    added seeding file contents

---
+++ modules/movies.js
@@ -0,0 +1,17 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database.js';
+
+export const Movie = sequelize.define('Movie', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  ratings: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  }
+});
